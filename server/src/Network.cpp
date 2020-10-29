1953945c3773eb595c3525500cca5ae9f4ccd889
lorris <lorris.hamdaoui@epitech.eu>
2020-10-29 16:32:52 +0100

    same as before

--- server/src/Network.cpp
+++ server/src/Network.cpp
@@ -17,12 +17,16 @@ void udp_server::start_receive()
     this->socket_.async_receive_from(boost::asio::buffer(this->recv_buffer_), this->remote_endpoint_, boost::bind(&udp_server::process, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
 }
 
-void udp_server::process(const boost::system::error_code& error, std::size_t /*bytes_transferred*/)
+void udp_server::process(const boost::system::error_code& error, std::size_t bytes_transferred)
 {
     std::cout << this->remote_endpoint_.address() << std::endl;
 
     boost::shared_ptr<std::string> message(new std::string("suuuce"));
-    std::cout << this->recv_buffer_.data() << std::endl;
+
+
+    std::cout << std::string(this->recv_buffer_.begin(), this->recv_buffer_.begin()+ bytes_transferred) << std::endl;
+
+
     boost::bind(&udp_server::handle_send, this, message, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred);
     start_receive();
 }
