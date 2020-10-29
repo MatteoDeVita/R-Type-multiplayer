43abd996e4774f49df827476de814525838326a4
lorris <lorris.hamdaoui@epitech.eu>
2020-10-29 17:20:19 +0100

    adding better thigs

--- server/src/Network.cpp
+++ server/src/Network.cpp
@@ -20,11 +20,12 @@ void udp_server::start_receive()
 void udp_server::process(const boost::system::error_code& error, std::size_t /*bytes_transferred*/)
 {
     std::cout << this->remote_endpoint_.address() << std::endl;
-
+    //traitement des infos recus et envoies des infos aux bons clients
     boost::shared_ptr<std::string> message(new std::string("suuuce"));
-    std::cout << this->recv_buffer_.data() << std::endl;
-    boost::bind(&udp_server::handle_send, this, message, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred);
-    start_receive();
+    std::cout << "datas received : "<< this->recv_buffer_.data() << std::endl;
+    socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_, boost::bind(&udp_server::handle_send, this, message,boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
 }
 
-void udp_server::handle_send(boost::shared_ptr<std::string> /*message*/, const boost::system::error_code& /*error*/, std::size_t /*bytes_transferred*/){}
+void udp_server::handle_send(boost::shared_ptr<std::string> /*message*/, const boost::system::error_code& /*error*/, std::size_t /*bytes_transferred*/){
+    std::cout << "datas sended to" << this->remote_endpoint_.address() << std::endl;
+}
