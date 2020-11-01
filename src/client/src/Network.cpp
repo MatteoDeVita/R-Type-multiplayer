fc0b52d4c0aee61ca3207725fcb1f8d334f36b23
lorris <lorris.hamdaoui@epitech.eu>
2020-11-01 01:34:46 +0100

    add master

--- src/client/src/Network.cpp
+++ src/client/src/Network.cpp
@@ -1,72 +1,38 @@
-/*
-** EPITECH PROJECT, 2020
-** B-CPP-501-LYN-5-1-rtype-lorris.hamdaoui [WSL: Ubuntu-20.04]
-** File description:
-** Network
-*/
+// /*
+// ** EPITECH PROJECT, 2020
+// ** B-CPP-501-LYN-5-1-rtype-lorris.hamdaoui [WSL: Ubuntu-20.04]
+// ** File description:
+// ** Thread
+// */
 
-#include "Network.hpp"
-#include "Debug_color.hpp"
+// #include "Network.hpp"
 
-void Threaded_Send(Network *ClassAccess)//envoie les actions du client -> A FAIRE : regler le tickRate 
-{
-    while(1) {
-        boost::asio::steady_timer timer1_(ClassAccess->_io, boost::asio::chrono::milliseconds(ClassAccess->ms_speed));//1000ms
-        timer1_.wait();
+// using boost::asio::ip::udp;
 
-        ClassAccess->NetStruct.a = "lorris est gay";
-        ClassAccess->NetStruct.b = 'a';
-        ClassAccess->NetStruct.c += 1;
+// Network::Network()
+// {
+   
+// }
 
-        std::ostringstream archive_stream;
-        boost::archive::text_oarchive archive(archive_stream);  
-        archive << ClassAccess->NetStruct;
-       
-        ClassAccess->_socket.send_to(boost::asio::buffer(archive_stream.str()),ClassAccess->_server_endpoint);//envoie de la struct au serv
-        std::cout << BOLDYELLOW << "[DATA SENDED]" << RESET << " -> DEST=" << ClassAccess->_server_endpoint << std::endl;//DEBUG
-    }
-}
+// Network::~Network()
+// {
+//     delete this->thread_send;
+//     delete this->thread_receive;
+// }
 
-void Threaded_Receive(Network *ClassAccess) //recoit et met a jour les datas du client
-{
-    std::string str;
-    str.resize(1024);
+// void Network::send(std::string message, udp::socket *socket)
+// {
+//     while(1) {
+//         boost::asio::steady_timer timer1_(this->io, boost::asio::chrono::seconds(1));
+//         timer1_.wait();
+//         socket->send_to(boost::asio::buffer(message), this->serverEndpoint);//envoie d'une var sendbuf au endpoint
+//     }
+// }
 
-    while(1) {
-        udp::endpoint client_endpoint;
-        size_t len = ClassAccess->_socket.receive_from(boost::asio::buffer(str), client_endpoint);
-
-        std::istringstream archive_stream(str);
-        boost::archive::text_iarchive archive(archive_stream);
-        archive >> ClassAccess->NetStruct;
-        std::cout << BOLDGREEN << "[DATA RECEIVED AND UPDATED]" << RESET <<  " -> FROM=" << ClassAccess->_server_endpoint << std::endl;//DEBUG
-
-     //   std::cout << "contenu de a : " << ClassAccess->NetStruct.a << std::endl; //TEST
-     //   std::cout << "contenu de b : " << ClassAccess->NetStruct.b << std::endl;//TEST
-     //   std::cout << "contenu de c : " << ClassAccess->NetStruct.c << std::endl;//TEST
-    }
-}
-
-Network::Network(int argc, char **argv) : _io(), _resolver(_io), _socket(_io)
-{
-    if (argc != 3) {
-        std::cerr << "Usage: client <host> <UDP packets speed in MS>" << std::endl;
-        exit(84);
-    }
-    this->ms_speed = atoi(argv[2]);
-    this->NetStruct.c = 0; //exemple
-
-    this->_server_endpoint = *_resolver.resolve(udp::v4(), argv[1], "3000").begin(); //recuperation du endpoint
-    this->_socket.open(udp::v4()); // ouverture du socket
-
-    boost::thread t1(Threaded_Send, this);
-    boost::thread t2(Threaded_Receive, this);
-    t1.detach();
-    t2.detach();
-    t1.join();
-    t2.join();
-}
-
-Network::~Network()
-{
-}
+// void Network::receive(udp::socket *socket) {
+//     while(1) {
+//         udp::endpoint client_endpoint;
+//         size_t len = socket->receive_from(boost::asio::buffer(this->recv_buf), client_endpoint);
+//         std::cout << "Data received : " << this->recv_buf.data() << std::endl;
+//     }
+// }
--- src/client/src/main.cpp
+++ src/client/src/main.cpp
@@ -5,14 +5,89 @@
 ** Main
 */
 
-#include "Network.hpp"
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
 
-int main(int argc, char **argv)
+//#include "Network.hpp"
+using boost::asio::ip::udp;
+
+typedef struct sample_t {
+    std::string a;
+    char b;
+    int c;
+    template<typename Ar> void serialize(Ar& ar, unsigned) { ar & a & b & c; }
+} sample;
+sample Networkstruct;
+void StartMessage(int argc) {
+    if (argc != 2) {
+        std::cerr << "Usage: client <host>" << std::endl;
+        exit(84);
+    }
+}
+
+void Receive(udp::socket *socket) //recoit et met a jour les datas du client -> A FAIRE : regler le tickRate 
 {
-    Network net(argc, argv);
+    std::string str;
+    str.resize(1024);
+
+    while(1) {
+        udp::endpoint client_endpoint;
+        size_t len = socket->receive_from(boost::asio::buffer(str), client_endpoint);
+
+        std::istringstream archive_stream(str);
+        sample mystruct;
+        boost::archive::text_iarchive archive(archive_stream);
+        archive >> mystruct;
+
+        std::cout << "contenu de a : " << mystruct.a  << std::endl;
+         std::cout << "contenu de b : " << mystruct.b << std::endl;
+          std::cout << "contenu de c : " << mystruct.c  << std::endl;
+    }
+}
+
+void Send(boost::asio::io_context *io, udp::endpoint *serverEndpoint, udp::socket *socket)//envoie les actions du client -> A FAIRE : regler le tickRate 
+{
+    while(1) {
+        boost::asio::steady_timer timer1_(*io, boost::asio::chrono::seconds(1));
+        timer1_.wait();
+
+        
+        Networkstruct.a = "lorris est gay";
+        Networkstruct.b = 'a';
+        Networkstruct.c += 1;
+
+        std::ostringstream archive_stream;
+        boost::archive::text_oarchive archive(archive_stream);  
+        archive << Networkstruct;
+
+        socket->send_to(boost::asio::buffer(archive_stream.str()), *serverEndpoint);//envoie de la struct au serv
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    StartMessage(argc);
+    Networkstruct.c = 0;
+    boost::asio::io_context io;
+    udp::resolver resolver(io);
+    udp::endpoint serverEndpoint = *resolver.resolve(udp::v4(), argv[1], "3000").begin(); //recuperation du endpoint
+    udp::socket socket(io); //creation d'un socket UDP
+    socket.open(udp::v4()); // ouverture du socket
+
+    boost::thread t1(Send, &io, &serverEndpoint, &socket);
+    boost::thread t2(Receive, &socket);
+    t1.detach();
+    t2.detach();
 
     while (1) { //Gameloop
     
     }
+
+    t1.join();
+    t2.join();
     return 0;
 }
